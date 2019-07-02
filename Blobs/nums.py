c1488b97ed150569a29627e90419c69505aac660
Spencer Felton <SpencerEFelton@gmail.com>
2019-07-02 13:35:21 +0900

    removing duplicate file

--- Blobs/nums.py
+++
@@ -1,8 +0,0 @@
-def number_printer(n):
-    new_n = [int(i) for i in str(n)]
-    for nums in new_n:
-        new_n[nums] += 1
-    new_n = int(''.join(map(str, new_n)))
-    return new_n
-
-print(number_printer(998))
